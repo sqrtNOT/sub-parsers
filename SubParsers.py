b96c541ec89bcac183244294d9db5b7fbd63fbb3
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2022-11-23 02:22:00 +0000

    Add vtt parser

--- SubParsers.py
+++ SubParsers.py
@@ -34,3 +34,24 @@ def parseass(path):
 def parsessa(path):
     return parseass(path)
 
+
+def parsevtt(path):
+    import re
+    document = open(path, 'r').read()
+    results = re.compile(r"""
+        (\d+)\s*?\n  # subtitle number
+        (\d+:\d+:\d+.\d+)\s*?-->\s*?(\d+:\d+:\d+.\d+)\s*  # start --> end
+        # optional parameters
+        (?:position:([^\n]+?))?
+        (?:align:([^\n]+?))?
+        (?:size:([^\n]+?))?
+        (?:line:([^\n]+))?
+        \n
+        ([\s\S]+?)  # subtitle text
+        (?=\s*?\d+\s*?\n\s*\d+:\d+:\d+.\d+\s*?-->|\Z)  # stop at next subtitle or end of document
+        """, re.VERBOSE).findall(document)
+    arr = []
+    for tup in results:
+        _dic = {"line": tup[0], "start": tup[1], "end": tup[2], "text": tup[-1].strip()}
+        arr.append(_dic)
+    return arr
